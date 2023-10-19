744161cb5005e52f4e887102ceb82a75fe474dee
jklpdgoat <jarpadul@gmail.com>
2023-10-19 22:17:50 +0800

    Sparse Matrix in C

---
+++ basic_ds/sparse_matrix.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+void main() {
+    static int array[10][10];
+    int i, j, m, n;
+    int counter = 0;
+
+    printf("Enter the order of the matrix \n");
+    printf("Enter the number of rows in array: ");
+    scanf("%d", &m);
+    printf("Enter the number of columns in array: ");
+    scanf("%d", &n);
+
+    printf("Enter the co-efficients of the matrix: \n");
+    for (i = 0; i < m; ++i) {
+        for (j = 0; j < n; ++j) {
+            scanf("%d", &array[i][j]);
+            if (array[i][j] == 0) {
+                ++counter;
+            }
+        }
+    }
+
+    if (counter > ((m *n) / 2)) {
+        printf("The given matrix is sparse matrix \n");
+    }
+
+    else
+        printf("The given matnix is not a sparse matrix \n");
+    printf("There are %d number of zeros\n", counter);
+}
